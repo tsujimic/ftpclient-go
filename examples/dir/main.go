989d801de79e70062b28b231deb7a1d04f06342a
Tsujimic <takashi.tsuji@icloud.com>
2016-07-08 06:54:26 +0900

    update examples

---
+++ examples/dir/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+    "fmt"
+    "flag"
+    "io"
+    "log"
+    "net"
+    "os"
+    "strconv"
+    "time"
+    "github.com/tsujimic/ftpclient-go"
+)
+
+func main() {
+    var host, user, pass, remote, tracefile string
+    var port int
+    flag.StringVar(&host, "host", "", "target host name")
+    flag.IntVar(&port, "port", 21, "tcp/ip port number")
+    flag.StringVar(&user, "user", "", "login username")
+    flag.StringVar(&pass, "pass", "", "login password")
+    flag.StringVar(&remote, "remote", "", "remote file path")
+    flag.StringVar(&tracefile, "trace", "", "trace file path")
+    flag.Parse()
+
+    if tracefile != "" {
+        file, err := os.OpenFile(tracefile, os.O_RDWR | os.O_CREATE | os.O_APPEND, 0666)
+        if err != nil {
+            panic(err)
+        }
+        defer file.Close()    
+        log.SetOutput(io.MultiWriter(file, os.Stderr))
+    }
+
+    defer func() {
+        if err := recover(); err != nil {
+            log.Println(err)
+        }
+    }()
+
+    log.Println("Start")
+    start := time.Now()
+    addr := net.JoinHostPort(host, strconv.Itoa(port))
+    client, err := ftpclient.DialTimeout(addr, time.Duration(30) * time.Second)
+    if err != nil {
+        panic(err)
+    }
+    defer client.Quit()
+   
+    err = client.Login(user, pass)
+    if err != nil {
+        panic(err)
+    }
+
+    client.SetPasv(true)
+    infos, err := client.Dir(remote)
+    if err != nil {
+        panic(err)
+    }
+    
+    for _, v := range infos {
+        log.Println(fmt.Sprintf(" Name: %s Size: %d", v.Name(), v.Size()))
+    }
+    
+    sec := (time.Now().Sub(start)).Seconds()
+    log.Printf("Stopwatch : %f seconds", sec)
+}
